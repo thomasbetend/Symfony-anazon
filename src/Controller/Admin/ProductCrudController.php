96ad28d6891262d959094eaea972f74238255bb6
Cedric LOMBARDOT <cedric@donkeycode.com>
2023-01-04 15:06:28 +0100

    Add category in crud

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Product;
 use App\Form\ProductImageType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
@@ -21,6 +22,7 @@ class ProductCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         yield FormField::addTab('Informations générales');
+        yield AssociationField::new('category')->autocomplete();
         yield Field::new('name');
         yield MoneyField::new('price')->setCurrency('EUR')->setStoredAsCents(false);
         yield TextEditorField::new('description')->hideOnIndex();
